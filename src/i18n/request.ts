0f81ac23cf5ae9d03d164f545580eab6d9f100e4
jerome <jerome@geekstudio.co.kr>
2025-12-22 10:59:46 +0900

    🔧 chore: 다국어 설정 파일 경로 변경

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -11,6 +11,6 @@ export default getRequestConfig(async () => {
 
     return {
         locale,
-        messages: (await import(`./i18n/${locale}.json`)).default,
+        messages: (await import(`./${locale}.json`)).default,
     };
 });
