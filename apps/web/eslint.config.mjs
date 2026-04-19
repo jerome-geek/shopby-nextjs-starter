d0aa81745fae0d994a0f6379b3b68cad18804ba8
jerome <jerome@geekstudio.co.kr>
2026-04-19 13:35:54 +0900

    📦 package: eslint 관련 패키지 추가 및 설정 추가

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
     ...nextVitals,
@@ -14,6 +14,14 @@ const eslintConfig = defineConfig([
         'next-env.d.ts',
     ]),
     {
+        files: ['**/src/**/*.{ts,tsx}'],
+        settings: {
+            react: {
+                version: '19',
+            },
+        },
+    },
+    {
         files: ['**/src/components/layout/**/*.{ts,tsx}'],
         rules: {
             'no-restricted-imports': 'off',
@@ -21,7 +29,10 @@ const eslintConfig = defineConfig([
     },
     {
         files: ['**/src/**/*.{ts,tsx}'],
-        ignores: ['**/src/components/layout/**/*.{ts,tsx}'],
+        ignores: [
+            '**/src/components/layout/**/*.{ts,tsx}',
+            // '**/src/hooks/queryOptions/index.ts',
+        ],
         rules: {
             // CDN 이미지 사용 시 next/image 불필요
             '@next/next/no-img-element': 'off',
@@ -31,8 +42,19 @@ const eslintConfig = defineConfig([
                     patterns: [
                         {
                             group: ['@/components/layout/*'],
-                            message: 'Please import from "@/components/layout" barrel instead of deep sub-folders for better consistency and performance.',
+                            message:
+                                'Please import from "@/components/layout" barrel instead of deep sub-folders for better consistency and performance.',
                         },
+                        // {
+                        //     group: [
+                        //         '@/hooks/queryOptions/*',
+                        //         '@/hooks/queryOptions/**',
+                        //         // '**/hooks/queryOptions/*',
+                        //         // '**/hooks/queryOptions/**',
+                        //     ],
+                        //     message:
+                        //         'queryOptions 폴더의 하위 파일에 직접 접근하지 마세요.',
+                        // },
                     ],
                 },
             ],
