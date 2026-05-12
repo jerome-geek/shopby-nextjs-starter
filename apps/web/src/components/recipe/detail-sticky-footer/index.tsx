65bb74677d630df69d134d2486b7b962bf98c062
Harper <163747140+sinheyy@users.noreply.github.com>
2026-05-12 15:20:11 +0900

    레시피 상세 하단바 공유버튼 추가 (#61)
    Resolves: #레몬트리/100

--- apps/web/src/components/recipe/detail-sticky-footer/index.tsx
+++ apps/web/src/components/recipe/detail-sticky-footer/index.tsx
@@ -1,7 +1,11 @@
-import { Bookmark, Heart, MessageCircle } from 'lucide-react';
+import { Bookmark, Heart, MessageCircle, Share2 } from 'lucide-react';
+import { overlay } from 'overlay-kit';
 
+import ShareBottomSheet from '@/components/bottom-sheet/share';
+import ShareModal from '@/components/modal/share';
 import * as styles from '@/components/recipe/detail-sticky-footer/index.css';
 import { useRecipeCommentList } from '@/hooks/suspenseQuery/shop/comment';
+import { useResponsive } from '@/hooks/utils';
 
 export { RecipeDetailStickyFooterSkeleton } from '@/components/recipe/detail-sticky-footer/skeleton';
 
@@ -26,12 +30,24 @@ export const RecipeDetailStickyFooter = ({
     onBookmarkToggle,
     onCommentClick,
 }: RecipeDetailStickyFooterProps) => {
+    const { isMobile } = useResponsive();
+
     const { data: recipeCommentListData } = useRecipeCommentList({
         params: {
             contentType: 'BOARD',
             contentSno: recipeSno,
         },
     });
+
+    const handleShareButtonClick = () => {
+        overlay.open((props) => {
+            return isMobile ? (
+                <ShareBottomSheet {...props} />
+            ) : (
+                <ShareModal {...props} />
+            );
+        });
+    };
 
     return (
         <footer className={styles.footer}>
@@ -55,18 +71,28 @@ export const RecipeDetailStickyFooter = ({
                 </button>
             </div>
 
-            <button
-                className={styles.actionButton}
-                onClick={onBookmarkToggle}
-                data-active={bookmarked}
-                data-type='bookmark'
-            >
-                <Bookmark
-                    size={24}
-                    fill={bookmarked ? 'currentColor' : 'none'}
-                />
-                <span>{bookmarkCount.toLocaleString()}</span>
-            </button>
+            <div className={styles.buttonContainer}>
+                <button
+                    type='button'
+                    onClick={handleShareButtonClick}
+                    aria-label='공유하기'
+                >
+                    <Share2 size={24} strokeWidth={1.5} />
+                </button>
+
+                <button
+                    className={styles.actionButton}
+                    onClick={onBookmarkToggle}
+                    data-active={bookmarked}
+                    data-type='bookmark'
+                >
+                    <Bookmark
+                        size={24}
+                        fill={bookmarked ? 'currentColor' : 'none'}
+                    />
+                    <span>{bookmarkCount.toLocaleString()}</span>
+                </button>
+            </div>
         </footer>
     );
 };
