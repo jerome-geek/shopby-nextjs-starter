ff03ec50b70daefacdb723fefa42d98c9baea2e7
jerome <jerome@geekstudio.co.kr>
2026-05-13 14:43:10 +0900

    🔧 chore: SEO 관련 태그 추가

--- apps/web/src/components/common/external-scripts.tsx
+++ apps/web/src/components/common/external-scripts.tsx
@@ -3,16 +3,25 @@ import Script from 'next/script';
 export function ExternalScripts() {
     return (
         <>
+            <meta
+                name='naver-site-verification'
+                content='1363febc455fbd821313d799e1f553a3dffdb1cc'
+            />
+            <meta
+                name='google-site-verification'
+                content='tltrrMHJ9cWS78-F9Mv0zbl8c0rjNuKBr60rmXdOPJI'
+            />
+
             {/* 샵바이 관련 */}
             <Script
-                id="myapp-script"
-                src="https://static.nhn-commerce.com/script/myapp.js"
-                strategy="afterInteractive"
+                id='myapp-script'
+                src='https://static.nhn-commerce.com/script/myapp.js'
+                strategy='afterInteractive'
             />
             <Script
-                id="shopby-external-script"
-                src="https://shopby-skin.cdn-nhncommerce.com/shopby-external-script.js"
-                strategy="afterInteractive"
+                id='shopby-external-script'
+                src='https://shopby-skin.cdn-nhncommerce.com/shopby-external-script.js'
+                strategy='afterInteractive'
             />
             {/* 기타 분석 도구 (GA, Pixel 등) */}
             {/* <Script id="google-analytics" ... /> */}
